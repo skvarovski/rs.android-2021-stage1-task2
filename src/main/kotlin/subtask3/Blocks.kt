437b000a19e2bc91c18831d5f670eb39f914573d
SkvarovskiXenon <skvarovski@gmail.com>
2021-06-20 02:10:48 +0300

    feat: solution robot

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,33 @@
 package subtask3
 
+import subtask1.DateFormatter
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+
+        // определяем тип класса
+        return when (blockB) {
+
+            String::class -> doMakeString(blockA)
+            Int::class -> doMakeInt(blockA)
+            LocalDate::class -> doMakeDate(blockA)
+            else -> throw Exception("Wrong blockB")
+        }
     }
-}
+
+    private fun doMakeString(blockA: Array<*>): String {
+        return blockA.filterIsInstance<String>().joinToString(separator = "")
+    }
+    private fun doMakeInt(blockA: Array<*>): Int {
+        return blockA.filterIsInstance<Int>().sum()
+    }
+    private fun doMakeDate(blockA: Array<*>): String {
+          return (blockA.filterIsInstance<LocalDate>().max() as LocalDate)
+              .format(DateTimeFormatter.ofPattern("dd.MM.yyyy",Locale("ru")))
+    }
+ }
