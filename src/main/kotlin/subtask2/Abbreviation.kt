be5eff347b35827a18ab0e2a84aa359890d66a85
SkvarovskiXenon <skvarovski@gmail.com>
2021-06-20 02:48:11 +0300

    feat: solution Abbreviation

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,28 @@
 package subtask2
 
+import java.util.*
+
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+
+        //переводив в прописные
+        val strA = a.toUpperCase(Locale("en"))
+        var strConcat = ""
+
+        // пробегаем и совпадающие откладываем во временную переменную
+        strA.forEach {
+            if (it in b) {
+                strConcat += it
+            }
+        }
+
+        // проверяем строки и длину
+        if (strConcat == b && strConcat.length == b.length) {
+            return "YES"
+        }
+
+        // ответ по-умолчанию
+        return "NO"
     }
 }
